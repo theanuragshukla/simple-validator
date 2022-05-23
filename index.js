ed63767608a73904cb654f674e4eaca16565b826
Anurag Shukla <tachyonanurag@gmail.com>
2022-05-23 21:19:12 +0530

    added exports

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-function checkSpaces(str, exact=true) {
+const checkSpaces=(str, exact=true)=> {
         var len = str.replace(/\s/g, '').length
         return (exact ? len === str.length && len !== 0: len !== 0)
 }
 
-function checklen(min,max,str){
+const checklen=(min,max,str)=>{
         if(!checkSpaces(str, true)){
                 return false;
         }else{
@@ -15,26 +15,26 @@ function checklen(min,max,str){
         }
 }
 
-function checkName(str){
+const checkName=(str)=>{
         return checklen(3, 50, str)
 }
 
-function validEmail(str){
+const validEmail=(str)=>{
         const atposition = str.indexOf("@");
         const dotposition = str.lastIndexOf(".");
         const wrongEmail = (atposition < 1 || dotposition < atposition+2 || dotposition+2 >= str.length || str.length <= 5);
         return !wrongEmail
 }
 
-function checkEmail(str){
+const checkEmail=(str)=>{
         return (checklen(8, 100, str) && validEmail(str) ? true : false )
 }
 
-function checkUser(str){
+const checkUser=(str)=>{
         const valid = /^[a-z0-9_\.]+$/.test(str);
         return (valid && checklen(8, 20, str) ? true : false);
 }
-function checkPass(str){
+const checkPass=(str)=>{
         var regularExpression = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
         if(regularExpression.test(str)){
                 return (checklen(8, 128, str)?true : false);
@@ -43,7 +43,7 @@ function checkPass(str){
         }
 }
 
-async function checkAll(arr){
+const checkAll=(arr)=>{
         const passError = !checkPass(arr.pass)
         const nameError = !checkName(arr.fname)
         const emailError = !checkEmail(arr.email)
@@ -53,4 +53,14 @@ async function checkAll(arr){
         }else{
                 return true
         }
+}
+
+module.exports={
+	checkUser,
+	checkEmail,
+	checkName,
+	checkPass,
+	checkAll,
+	checklen,
+	checkSpaces
 }
